3d063e327a68eb20fc2fb0df10abc41fb18ce1fb
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-21 13:58:38 -0300

    feat: initial setup

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+dayjs.extend(utc);
+
+app.use(
+    express.json(), cors(), express.urlencoded({ extended: true })
+);
+
+app.all("*", (req, res) => {
+    res.status(400).json("Not found!");
+});
+
+app.use(async (err, req, res, next) => {
+    if (err) {
+        res.status(500).json({
+            message: err.message,
+            code: 500
+        });
+
+        return;
+    }
+
+    res.status(404).json({
+        ermessage: "Not found!",
+        code: 404
+    });
+});
+
+app.listen(process.env.PORT_SERVER, () => {
+    console.log(`Listening on http://localhost:${process.env.PORT_SERVER}/`);
+});
