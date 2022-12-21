314c098ac8f3b0bbc35e9059fe0e2346f5790784
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-21 14:16:49 -0300

    feat: database configuration | initial utils

---
+++ src/configs/db.config.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv").config();
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.PORT
+}
+
+module.exports = dbConfig;
