e815b845e096f099004de2414ff9c80bb4d3d919
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-23 14:21:48 -0300

    feat: add food route

--- src/routes/foods/foods.route.js
+++ src/routes/foods/foods.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const routerFoods = express.Router();
 
-routerFoods.get("/Foods", (req, res) => {
-    res.send("Foods!");
-});
+const foodsController = require("../../controllers/food/foods.controller");
+
+routerFoods.get("/", foodsController.getAll);
+routerFoods.get("/:id", foodsController.getOne);
+routerFoods.post("/", foodsController.create);
+routerFoods.put("/:id", foodsController.update);
+routerFoods.delete("/:id", foodsController.deleteOne);
 
 module.exports = routerFoods;
