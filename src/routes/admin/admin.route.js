85e4a06df37aa5f31e9ae7ea642bab04e9393f57
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-25 09:28:33 -0300

    feat: adding administrative route

--- src/routes/admin/admin.route.js
+++ src/routes/admin/admin.route.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const routerAdmin = express.Router();
 
-routerAdmin.get("/Admin", (req, res) => {
-    res.send("Admin!");
-});
+const adminController = require("../../controllers/admin/admin.controller");
+const { authRole } = require("../../middlewares/protected.middleware");
+const { ROLE } = require("../../utils/consts");
+
+routerAdmin.use(authRole(ROLE.ADMIN));
+
+routerAdmin.get("/users", adminController.getAllUsers);
+routerAdmin.get("/users/:id", adminController.getUser);
+routerAdmin.get("/users/:id/foods", adminController.getUserFoods);
+
+routerAdmin.post("/users/:id/foods", adminController.createUserFood);
+routerAdmin.put("/users/:id/foods/:foodId", adminController.updateUserFood);
+routerAdmin.delete("/users/:id/foods/:foodId", adminController.deleteUserFood);
+routerAdmin.get("/statistics", adminController.getStatistics);
+
+routerAdmin.post("/random/users", adminController.createRandomUsers);
+routerAdmin.post("/random/foods", adminController.createRandomFoodsForUser);
 
 module.exports = routerAdmin;
